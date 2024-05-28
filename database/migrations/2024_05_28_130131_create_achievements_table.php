363fb34d45a43cc9fa1b4ec7f35dc931f97223c3
jerem-marti <jeremy_martin@bluewin.ch>
2024-05-28 16:52:45 +0200

    Fix : Achivements table

--- database/migrations/2024_05_28_130131_create_achievements_table.php
+++ database/migrations/2024_05_28_130131_create_achievements_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('achievements', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('icon');
             $table->timestamps();
         });
     }
