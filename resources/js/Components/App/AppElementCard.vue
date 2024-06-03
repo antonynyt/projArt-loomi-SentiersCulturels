ad6a97c5a905b80bf8783146794650e7eabfe2df
Antony Neyret <antony.neyret@gmail.com>
2024-06-03 09:24:33 +0200

    front: implement path card

---
+++ resources/js/Components/App/AppElementCard.vue
@@ -0,0 +1,58 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+
+const props = defineProps({
+    thumbnail: {
+        type: String,
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    href: {
+        type: String,
+        required: true,
+    },
+    location: {
+        type: String,
+        required: true,
+    },
+    infos: {
+        type: Object,
+        required: false,
+        distance: {
+            type: String,
+            required: true,
+        },
+        duration: {
+            type: String,
+            required: true,
+        },
+        elevation: {
+            type: String,
+            required: true,
+        },
+    }
+});
+
+</script>
+
+<template>
+    <article class="border border-midnight-blue rounded-2xl p-1">
+        <Link :href="href" class="flex justify-between" preserve-state>
+            <img :src="thumbnail" :alt="title" class="object-cover rounded-lg aspect-square h-24">
+            <div class="grow flex flex-col justify-center px-5">
+                <h3 class="text-lg font-medium text-midnight-blue">
+                    <a :href="href">{{ title }}</a>
+                </h3>
+                <p class="text-sm text-gray-500">{{ location }}</p>
+                <div v-if="infos" class="flex flex-row justify-between mt-2">
+                    <p class="text-sm text-gray-500">{{ infos.distance }}</p>
+                    <p class="text-sm text-gray-500">{{ infos.duration }}</p>
+                    <p class="text-sm text-gray-500">{{ infos.elevation }}</p>
+                </div>
+            </div>
+        </Link>
+    </article>
+</template>
