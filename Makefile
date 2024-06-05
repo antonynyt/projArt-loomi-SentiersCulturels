d394d20e170e0b5353cf90d3b5324ad5c3b02630
ariameli <ariadne.melissargos@heig-vd.ch>
2024-06-05 09:31:00 +0200

    navbar to fav page & profile page + titles to same pages

--- Makefile
+++ Makefile
@@ -14,7 +14,7 @@ copy-env:
 
 generate-key:
 	@echo "Generating application key..."
-	php artisan migrate:fresh --seed
+	php artisan key:generate
 
 start-mysql:
 	@echo "Starting MySQL database server with Docker..."
--- resources/js/Pages/Favorites.vue
+++ resources/js/Pages/Favorites.vue
@@ -1,10 +1,17 @@
 <script setup>
-
-
+import { Head } from "@inertiajs/vue3";
+import TheHeader from "@/Components/App/TheHeader.vue";
+import DefaultLayout from "@/Layouts/DefaultLayout.vue";
+import ContentLayout from "@/Layouts/ContentLayout.vue";
 </script>
 
 <template>
-    <div>
-        <h1>Favorites</h1>
-    </div>
+    <Head>
+        <title>Favoris</title>
+    </Head>
+    <DefaultLayout>
+        <ContentLayout>
+            <TheHeader title="Favoris" class="mb-8" />
+        </ContentLayout>
+    </DefaultLayout>
 </template>
