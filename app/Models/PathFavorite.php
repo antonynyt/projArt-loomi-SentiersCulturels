c94739b49100e3f1c80f066fea30992190eedb2b
jerem-marti <jeremy_martin@bluewin.ch>
2024-06-06 10:44:59 +0200

    fix: PathFavorite model

--- app/Models/PathFavorite.php
+++ app/Models/PathFavorite.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Path;
+use App\Models\User;
 
 class PathFavorite extends Model
 {
