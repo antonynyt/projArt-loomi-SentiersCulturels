a8ab8fb0cb3b0a8029bff66243b2cb5cbdbe3048
Francois Cuennet <francois.cuennet@gmail.com>
2024-06-06 10:37:03 +0200

    sauvegarde pour fetch le main avec les favoris dans la database

--- resources/js/Pages/Favorites.vue
+++ resources/js/Pages/Favorites.vue
@@ -1,10 +1,35 @@
 <script setup>
 
+import { Head } from '@inertiajs/vue3';
+import ContentLayout from '@/Layouts/ContentLayout.vue';
+import AppElementCard from '@/Components/App/AppElementCard.vue';
+import TheHeader from '@/Components/App/TheHeader.vue';
+
+
 
 </script>
 
+
 <template>
-    <div>
-        <h1>Favorites</h1>
-    </div>
+    <Head>
+        <title>Favorite</title>
+    </Head>
+
+    <ContentLayout>
+        <TheHeader title="Favorite" class="mb-8"/>
+
+        <div>
+            <AppElementCard
+                v-for="(card, index) in cards"
+                :key="index"
+                @click="toggleDrawer"
+                :thumbnail="card.thumbnail"
+                :title="card.title"
+                :href="card.href"
+                :location="card.location"
+                :infos="card.infos"
+            />
+        </div>
+    </ContentLayout>
+    
 </template>
