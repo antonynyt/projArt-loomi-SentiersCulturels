f7a9d7e6f688e62139b5d16c50ccf3e0081e3780
Francois Cuennet <francois.cuennet@gmail.com>
2024-06-13 11:00:19 +0200

    add: route onboarding

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ExplorerController;
+use App\Http\Controllers\LancementController;
 use App\Http\Controllers\MapController;
 use App\Http\Controllers\PathController;
 use App\Http\Controllers\PoiController;
@@ -18,6 +19,7 @@ use App\Http\Controllers\AchievementController;
 
 //ACCUEIL & EXPLORER
 Route::get('/', [ExplorerController::class, 'index'])->name('explorer.index');
+Route::get('/lancement', [LancementController::class, 'index'])->name('lancement.index');
 
 //CARTE
 Route::get('/map', [MapController::class, 'index'])->name('map.index');
@@ -61,14 +63,22 @@ Route::group(['middleware' => 'role:user'], function () {
 
 });
 
-Route::group(['middleware'=>'role:editor'],function(){
-    Route::get('/new-path', function () { return redirect('new-path/instructions'); });
-    Route::get('/new-path/{otherThanExistant}', function () { return redirect('new-path/instructions'); })->where('otherThanExistant', '^(?!instructions$|success$|form$).*');
-    Route::get('/new-path/instructions', function () { return Inertia::render('NewPath/NewPathInstructions');});
+Route::group(['middleware' => 'role:editor'], function () {
+    Route::get('/new-path', function () {
+        return redirect('new-path/instructions');
+    });
+    Route::get('/new-path/{otherThanExistant}', function () {
+        return redirect('new-path/instructions');
+    })->where('otherThanExistant', '^(?!instructions$|success$|form$).*');
+    Route::get('/new-path/instructions', function () {
+        return Inertia::render('NewPath/NewPathInstructions');
+    });
     Route::post('/new-path/map', [NewPathController::class, 'map'])->name('new-path.map');
     Route::post('/new-path/search', [NewPathController::class, 'search']);
     Route::post('/new-path/form', [NewPathController::class, 'form']);
-    Route::get('/new-path/form', function () { return Inertia::render('NewPath/NewPathForm'); });
+    Route::get('/new-path/form', function () {
+        return Inertia::render('NewPath/NewPathForm');
+    });
     Route::post('/new-path/create', [NewPathController::class, 'create']);
 });
 
